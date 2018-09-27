c3ac0df4fb345cc30ab8b2b0bc2e893341cba6ca
Pooja Patel <poojarkpatel0397@gmail.com>
2018-09-27 19:52:28 +0530

    displayMenu and performOperations

---
+++ BankSystemClass.py
@@ -0,0 +1,9 @@
+from BankingSystem import BankingSystem
+
+def main():
+
+	bankSystem = BankingSystem ()
+	bankSystem.displayMenu()
+
+if __name__ == '__main__':
+	main()
--- BankingSystem.py
+++ BankingSystem.py
@@ -1,1 +1,73 @@
-	
+
+class BankingSystem:
+
+	def __init__(self):
+		pass
+
+
+
+	def displayMenu(self):
+		print("===============BANKING SYSTEM===================")
+		print("1. ADD ACCOUNT")
+		print("2. VIEW ACCOUNT")
+		print("3. CREDIT")
+		print("4. DEBIT")
+		print("5. TRANSACTION")
+		print("6. DELETE ACCOUNT")
+		print("7. EXIT")
+		self.__choice = int(input("Enter your choice :- "))
+		self.__performOPerations(self.__choice)
+		self.displayMenu()
+
+
+	def __performOPerations(self,choice):
+		if choice is 1:
+			name,address,date,month,year,branchName,gender = self.__getUserDetails()
+			print(gender)
+			print("this is add account block")
+		elif choice is 2:
+			print("this is view account block")
+		elif choice is 3:
+			print("credit block")
+		elif choice is 4:
+			print("debit block")
+		elif choice is 5:
+			print("TRANSACTION")
+		elif choice is 6:
+			print("delete account")
+		elif choice is 7:
+			raise SystemExit
+		else:
+			print("invalid input")
+
+	def __getUserDetails(self):
+		name = input("Enter your name :- ")
+		address = input("Enter your address :- ")
+		date = int(input("Enter date :- "))
+		month = int(input("Enter month :- "))
+		year = int(input("Enter year :- "))
+		ifsc = input("1.Motera 2.Science City 3.Sahibaug \n Choose your Branch :-")
+		if ifsc =='1':
+			branchName = 'Motera'
+		if ifsc =='2':
+			branchName = 'Science City'
+		if ifsc =='3':
+			branchName = 'sahibaug'
+
+		gender = input("1.Male 2.Female \n Choose your Gender :-")
+		if gender =='1':
+			gender = 'Male'
+		if gender =='2':
+			gender = 'Female'
+
+		return name,address,date,month,year,branchName,gender
+
+
+
+
+
+
+
+
+		
+
